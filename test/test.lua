806a009c63cc076feca87ab4abde8a527a35246e
lafar6502 <rafal@cogit.pl>
2013-04-19 22:43:39 +0200

    capturing TCP and UDP. showing TCP payload

--- test/test.lua
+++ test/test.lua
@@ -33,11 +33,62 @@ typedef struct udp_header{
     uint16_t len;            // Datagram length
     uint16_t crc;            // Checksum
 } udp_header;
-]]
 
+typedef struct udp_header2{
+    uint8_t sport[2];          // Source port
+    uint8_t dport[2];          // Destination port
+    uint8_t len[2];            // Datagram length
+    uint8_t crc[2];            // Checksum
+} udp_header2;
+
+typedef struct tcp_header {
+    uint16_t sport;   /* source port */
+    uint16_t dport;   /* destination port */
+    uint32_t seq;     /* sequence number */
+    uint32_t ack;     /* acknowledgement number */
+    uint8_t  offx2;    /* data offset, rsvd */
+    uint8_t  flags;
+    uint16_t win;     /* window */
+    uint16_t sum;     /* checksum */
+    uint16_t urp;     /* urgent pointer */
+} tcp_header;
+]]
+--[[#define TH_OFF(th)  (((th)->th_offx2 & 0xf0) >> 4)
+    #define TH_FIN 0x01
+    #define TH_SYN 0x02
+    #define TH_RST 0x04
+    #define TH_PUSH 0x08
+    #define TH_ACK 0x10
+    #define TH_URG 0x20
+    #define TH_ECE 0x40
+    #define TH_CWR 0x80
+    #define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+]]
+    
+function printx(x)
+  print("0x"..bit.tohex(x))
+end
 function addrs(ip_addr)
     return ""..ip_addr.byte1.."."..ip_addr.byte2.."."..ip_addr.byte3.."."..ip_addr.byte4
 end
+function bswap_bytes(p)
+    local t = p[0]
+    p[0] = p[1]
+    p[1] = t
+end
+function TH_OFF(th)
+    return bit.rshift(bit.band(th.offx2, 0xf0), 4)
+end
+
+
+local ntohl = ffi.abi("le") and bit.bswap or function(x) return x end
+function swap16(n)
+    return bit.band(bit.bor(bit.lshift(n, 8), bit.rshift(bit.band(n, 0xff00), 8)), 0xffff)
+end
+local ntohs = ffi.abi("le") and swap16 or function(x) return x end
+
+
+printx(ntohs(0x1234))
 
 local devs = pcap.findalldevs()
 print("Devices found: "..#devs)
@@ -46,7 +97,7 @@ for k,v in ipairs(devs) do
 end
 
 local pc = pcap.open_live(devs[4].name)
-pc:set_filter("ip and udp")
+pc:set_filter("ip and (udp or tcp)")
 --[[
 pc:loop(1000, function(data, ts, len)
     print("ts: "..ts..", len: "..len)
@@ -58,9 +109,26 @@ for pkt, ts, len in pc.next, pc do
     print("ts: "..ts..", len: "..len)
     local data = ffi.cast("char *", pkt)
     local hdr = ffi.cast("ip_header *", data + 14)
-    print("ADDR: "..addrs(hdr.saddr).." -> "..addrs(hdr.daddr))
+    
     local ip_len =  bit.band(hdr.ver_ihl, 0xf) * 4
-    print("IP len: "..ip_len)
-    local udphdr = ffi.cast("udp_header *", data + 14 + ip_len)
-    print("sport: "..udphdr.sport..", dport:"..udphdr.dport)
+    
+    if hdr.proto == 6 then
+        local tcphdr = ffi.cast("tcp_header*", data + 14 + ip_len)
+        local size_tcp = TH_OFF(tcphdr)*4;
+        print(addrs(hdr.saddr)..":"..tcphdr.sport.." -> "..addrs(hdr.daddr)..":"..tcphdr.dport.." TCP ")
+        print('size tcp is '..size_tcp)
+        local payload = data + 14 + ip_len + size_tcp;
+        local size_payload = ntohs(hdr.tlen) - (14 + size_tcp);
+        print('payload is '..payload[0]..' '..payload[1]..' '..payload[2])
+        local content = ffi.string(payload, size_payload)
+        print(content)
+    elseif hdr.proto == 17 then
+        local udphdr = ffi.cast("udp_header2 *", data + 14 + ip_len)
+        bswap_bytes(udphdr.sport)
+        bswap_bytes(udphdr.dport)
+        udphdr = ffi.cast("udp_header*", udphdr)
+        print(addrs(hdr.saddr)..":"..udphdr.sport.." -> "..addrs(hdr.daddr)..":"..udphdr.dport.." UDP ")
+        
+    else
+    end
 end
